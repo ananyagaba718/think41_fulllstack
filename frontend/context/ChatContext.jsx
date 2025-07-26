28bc03e09ad20617aaf4d3181c959085561b0d13
Ananya Gauba <141585260+ananyagaba718@users.noreply.github.com>
2025-07-26 12:07:32 +0530

    Update ChatContext.jsx

--- frontend/context/ChatContext.jsx
+++ frontend/context/ChatContext.jsx
@@ -1,1 +1,33 @@
+import { createContext, useContext, useState } from "react";
 
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [conversationId, setConversationId] = useState(null);
+  const [history, setHistory] = useState([]);
+
+  return(
+    <ChatContext.Provider
+      value={{
+        messages,
+        setMessages,
+        loading,
+        setLoading,
+        userInput,
+        setUserInput,
+        conversationId,
+        setConversationId,
+        history,
+        setHistory
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChat = () => useContext(ChatContext);
+        
