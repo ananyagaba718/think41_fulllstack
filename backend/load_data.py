6763b5bf4fd1a9b19f78c63c237c5839f2c2b8ea
Ananya Gauba <141585260+ananyagaba718@users.noreply.github.com>
2025-07-26 11:20:02 +0530

    Update load_data.py

--- backend/load_data.py
+++ backend/load_data.py
@@ -5,19 +5,20 @@ from models import *
 Base.metadata.create_all(bind=engine)
 
 def load_csv_data():
-  db = SessionLocal()
-  for file, model in [
-    ("distribution_centers.csv", DistributionCenter),
-    ("products.csv", Product),
-    ("inventory_items.csv", InventoryItem),
-    ("orders.csv", Order),
-    ("order_items.csv", OrderItem),
-    ("users.csv", User),
-  ]:
-    df=pd.read_csv(file)
-    for row in df.to_dict(orient="records"):
-      db.add(model(**row))
-  db.commit()
-db.close()
+    db = SessionLocal()
+    for file, model in [
+        ("distribution_centers.csv", DistributionCenter),
+        ("products.csv", Product),
+        ("inventory_items.csv", InventoryItem),
+        ("orders.csv", Order),
+        ("order_items.csv", OrderItem),
+        ("users.csv", User),
+    ]:
+        df = pd.read_csv(file)
+        for row in df.to_dict(orient="records"):
+            db.add(model(**row))
+        db.commit()
+    db.close()
+  
 if__name__== "__main__":
   load_csv_data()
