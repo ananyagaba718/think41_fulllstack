9bc931ee3d397b06b6fc290288bdaba523689617
Ananya Gauba <141585260+ananyagaba718@users.noreply.github.com>
2025-07-26 11:05:42 +0530

    database setup completed

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "postresql://postgres:password@localhost:5432/ai_chat"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
+Base = declarative_base()
