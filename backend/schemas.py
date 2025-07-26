9ac2d533e813e1e42885bc6a314f2961278e33fc
Ananya Gauba <141585260+ananyagaba718@users.noreply.github.com>
2025-07-26 11:25:35 +0530

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
+from typinf import Optional, List
 
+class ChatRequest(BaseModel):
+  user_id: int
+  message: str
+  conversation_id: Optional[int] = None
+
+class MessageSchema(BaseModel):
+  sender: str
+  message: str
+
+class ChatResponse(BaseModel):
+  coversation_id: int
+  message: List[MessageSchema]
+  
