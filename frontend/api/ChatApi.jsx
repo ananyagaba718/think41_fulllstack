aff5c6712a139ee099483e2e66e5a3a7d905a061
Ananya Gauba <141585260+ananyagaba718@users.noreply.github.com>
2025-07-26 12:13:02 +0530

    Update ChatApi.jsx

--- frontend/api/ChatApi.jsx
+++ frontend/api/ChatApi.jsx
@@ -1,1 +1,14 @@
-
+export async function sendMessageToApi({ message, userId, conversationId}) {
+  const res = await fetch("http://localhost:8000/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      user_id: userId,
+      message,
+      conversation_id: conversationId
+    })
+  });
+  return res.json();
+}
